002073d95a86884d677891a60f3944b11c55b516
Mali Michael <malimccalla@gmail.com>
2018-04-18 11:00:59 +0100

    Set up local mongo instance

--- client/src/components/blogs/BlogNew.js
+++ client/src/components/blogs/BlogNew.js
@@ -5,33 +5,24 @@ import BlogForm from './BlogForm';
 import BlogFormReview from './BlogFormReview';
 
 class BlogNew extends Component {
-  state = { showFormReview: false };
+  constructor(props) {
+    super(props);
+    this.state = { showFormReview: false };
+  }
 
   renderContent() {
     if (this.state.showFormReview) {
-      return (
-        <BlogFormReview
-          onCancel={() => this.setState({ showFormReview: false })}
-        />
-      );
+      return <BlogFormReview onCancel={() => this.setState({ showFormReview: false })} />;
     }
 
-    return (
-      <BlogForm
-        onBlogSubmit={() => this.setState({ showFormReview: true })}
-      />
-    );
+    return <BlogForm onBlogSubmit={() => this.setState({ showFormReview: true })} />;
   }
 
   render() {
-    return (
-      <div>
-        {this.renderContent()}
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
 export default reduxForm({
-  form: 'blogForm'
+  form: 'blogForm',
 })(BlogNew);
