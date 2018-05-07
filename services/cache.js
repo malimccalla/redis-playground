d694682b40867ab4b060eb60f2f449117a6caaa4
Mali Michael <malimccalla@gmail.com>
2018-05-07 11:41:14 +0100

    Set and get redis hash keys

---
+++ services/cache.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+const exec = mongoose.Query.prototype.exec;
+
+client.hget = util.promisify(client.hget);
+
+mongoose.Query.prototype.cache = async function(options = {}) {
+  this._cache = true;
+  this._hashKey = JSON.stringify(options.key || '');
+  // make sure cache function is chainable
+  return this;
+};
+
+mongoose.Query.prototype.exec = async function() {
+  if (!this._cache) return exec.apply(this, arguments);
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  const cacheValue = await client.hget(this._hashKey, key);
+
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments);
+
+  client.hset(this._hashKey, key, JSON.stringify(result), 'EX', 10);
+
+  return result;
+};
